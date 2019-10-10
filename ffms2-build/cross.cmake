09d6aded4a5e03834299a9a6628e6d7b5c97d912
Luni-4 <luni-4@hotmail.it>
2019-10-10 15:32:36 +0200

    Add Windows deployment

---
+++ ffms2-build/cross.cmake
@@ -0,0 +1,16 @@
+# This one is important
+SET(CMAKE_SYSTEM_NAME Windows)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+SET(CMAKE_ASM_YASM_COMPILER yasm)
+
+# -fPIC
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+SET(CMAKE_CXX_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+SET(CMAKE_C_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
