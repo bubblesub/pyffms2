6874aa6748e951757ba582a9eef2cdf847a1c6f5
abakum <koka.abakum@gmail.com>
2020-06-04 22:30:41 +0300

    For MINGW  search libffms2-?.dll in /bin
    `pacman -S ${MINGW_PACKAGE_PREFIX}-ffms2` place libffms2-?.dll to /bin

--- ffms2/get_library.py
+++ ffms2/get_library.py
@@ -86,7 +86,7 @@ if os.name == "nt":
         except NameError:
             script_dirs = []
         script_dirs.append(absdir(sys.argv[0]))
-        if "[GCC" in sys.version: # `pacman -S ${MINGW_PACKAGE_PREFIX}-ffms2` place libffms2-?.dll to /bin
+        if "[GCC" in sys.version: # in MINGW ffms2.dll with name libffms2-?.dll located in /bin
             script_dirs.append(absdir(sys.executable))
             win_formats+=["lib{}%s.dll"%(x or "") for x in range(0, -9, -1)]
         for win_format in win_formats:
